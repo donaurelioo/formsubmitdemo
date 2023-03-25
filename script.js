bd8ddde4e1841833f203d9747ec2d31584d2c361
donaurelioo <128910743+donaurelioo@users.noreply.github.com>
2023-03-25 14:38:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("from script file");
+//------Menu-script-----//
+var navLinks = document.getElementById("navLinks");
+
+function showMenu(){
+    navLinks.style.right ="0";
+}
+function hideMenu(){
+    navLinks.style.right="-200px";
+}
